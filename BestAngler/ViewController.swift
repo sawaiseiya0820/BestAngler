9b13bc14a6d81a227dfe5ce9c56fc418a0681d03
澤井聖也 <softtennis.s.s@icloud.com>
2016-03-25 17:07:22 +0900

    Initial Commit

---
+++ BestAngler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BestAngler
+//
+//  Created by 澤井聖也 on 2016/03/25.
+//  Copyright © 2016年 澤井聖也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
